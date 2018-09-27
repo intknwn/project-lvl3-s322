ebbe34953dc593ee11c5bf1de21c2ee3f4feb9d1
intknwn <intknwn@gmail.com>
2018-09-27 19:42:34 +0800

    Fixed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,21 +11,19 @@ const wrongUrl = `${host}/wrong`;
 const filePath = `${__dirname}/__fixtures__/boilerplate/index.html`;
 const newFilePath = `${__dirname}/__fixtures__/boilerplate/index2.html`;
 
-const fileData = fs.readFileSync(filePath, 'utf8');
-const newFileData = fs.readFileSync(newFilePath, 'utf8');
 const resources = [
   'img-hexlet.jpg',
   'js-main.js',
   'css-main.css',
 ];
 
-
 describe('page-loader', () => {
   let output;
+  const fileData = fs.readFileSync(filePath, 'utf8');
+  const newFileData = fs.readFileSync(newFilePath, 'utf8');
 
   beforeEach(() => {
     output = fs.mkdtempSync(tmpDir);
-
     nock.disableNetConnect();
 
     nock(host)
@@ -39,52 +37,30 @@ describe('page-loader', () => {
       .replyWithFile(200, `${__dirname}/__fixtures__/boilerplate/js/main.js`);
   });
 
-  // it('downloading page', async (done) => {
-  //   try {
-  //     const filePath = await loadPage(pageUrl, output);
-  //     expect(fileName).toBe('hexlet-io-courses.html');
-  //     const fileDir = `${output}${path.sep}${fileName}`;
-  //     const data = await fs.readFile(fileDir, 'utf8');
-  //     expect(data).toBe(fileData);
-  //     done();
-  //   } catch (err) {
-  //     done.fail(err);
-  //   }
-  // });
-
-  it('non-existent page', async (done) => {
+  it('non-existent page', async () => {
     try {
       await loadPage(wrongUrl, output);
-      done.fail();
     } catch (err) {
       expect(err.status).toBe(404);
-      done();
     }
   });
 
-  it('non-existent output directory', async (done) => {
+  it('non-existent output directory', async () => {
     try {
       await loadPage(pageUrl, 'notexist');
-      done.fail();
     } catch (err) {
       expect(err.code).toBe('ENOENT');
-      done();
     }
   });
 
-  it('downloading page and resources', async (done) => {
-    try {
-      const fileName = await loadPage(pageUrl, output);
-      expect(fileName).toBe('hexlet-io-courses.html');
-      const result1 = await fs.exists(path.resolve(output, fileName));
-      expect(result1).toBe(true);
-      const data = await fs.readFile(path.resolve(output, fileName), 'utf8');
-      expect(data).toBe(newFileData);
-      const result2 = await resources.map(item => fs.exists(path.resolve(output, 'hexlet-io-courses_files', item)));
-      expect(result2).not.toContain(false);
-      done();
-    } catch (err) {
-      done.fail(err);
-    }
+  it('downloading page and resources', async () => {
+    const fileName = await loadPage(pageUrl, output);
+    expect(fileName).toBe('hexlet-io-courses.html');
+    const fileExists = await fs.exists(path.resolve(output, fileName));
+    expect(fileExists).toBe(true);
+    const data = await fs.readFile(path.resolve(output, fileName), 'utf8');
+    expect(data).toBe(newFileData);
+    const resExist = await resources.map(item => fs.exists(path.resolve(output, 'hexlet-io-courses_files', item)));
+    expect(resExist).not.toContain(false);
   });
 });
